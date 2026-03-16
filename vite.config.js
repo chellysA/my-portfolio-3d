53a524b129994bfa32b0484b7a75b18e45a0fcfd
chellysA <chellys.castillo@gmail.com>
2026-03-16 17:37:49 -0400

    dev: title and link tag customized

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,25 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  name: "Chellys",
+  description: "Portfolio de Chellys",
+  keywords: [
+    "Portfolio",
+    "Chellys",
+    "Developer",
+    "Web Developer",
+    "React Developer",
+    "Vue Developer",
+    "Next.js Developer",
+    "Typescript Developer",
+    "3D Modelling Developer",
+    "Mobile Developer",
+  ],
+  author: "Chellys",
+  url: "https://chellys.com",
+  image: "https://chellys.com/images/logo.png",
+  icon: "https://chellys.com/images/logo.png",
+  twitter: {
+    card: "summary_large_image",
+  },
 });
