c5a567b8f1caa37dd945c5b2e46dc5a767c2bab7
chellysA <chellys.castillo@gmail.com>
2026-03-16 16:01:21 -0400

    fix: certifications importations

--- src/constants/certifications.js
+++ src/constants/certifications.js
@@ -1,32 +1,37 @@
+import responsiveWebDesign from "../assets/certifications/responsiveWebDesign.png";
+import javascript from "../assets/certifications/javascript.png";
+import frontEnd from "../assets/certifications/frontEnd.png";
+import bilingual from "../assets/certifications/bilingual.jpg";
+import bachiller from "../assets/certifications/bachiller.jpg";
 export const certifications = [
   {
-    img: "src/assets/certifications/responsiveWebDesign.png",
+    img: responsiveWebDesign,
     title: "Responsive Web Design",
     desc: "HTML, CSS, JavaScript and modern frontend tooling.",
     url: "https://www.freecodecamp.org/certification/chellys/responsive-web-design",
   },
   {
-    img: "src/assets/certifications/javascript.png",
+    img: javascript,
     title: "JavaScript",
     desc: "Data structures and problem solving with JavaScript.",
     url: "https://www.freecodecamp.org/certification/chellys/javascript-algorithms-and-data-structures",
   },
   {
-    img: "src/assets/certifications/frontEnd.png",
+    img: frontEnd,
     title: "Frontend Developer",
     desc: "Frontend Development and Libraries.",
     url: "https://www.freecodecamp.org/certification/chellys/front-end-development-libraries",
   },
   {
-    img: "src/assets/certifications/bilingual.jpg",
+    img: bilingual,
     title: "Bilingual",
     desc: "English.",
-    url: "src/assets/certifications/bilingual.jpg",
+    url: bilingual,
   },
   {
-    img: "src/assets/certifications/bachiller.jpg",
+    img: bachiller,
     title: "Procesamiento de Datos",
     desc: "Algoritmos y programación.",
-    url: "src/assets/certifications/bachiller.jpg",
+    url: bachiller,
   },
 ];
